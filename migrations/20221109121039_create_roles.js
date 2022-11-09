2d3b62443128c2e64e06e6494a8b13c1ce0ff653
brunosm16 <brunoskr23@gmail.com>
2022-11-09 09:12:49 -0300

    feat: create_roles migration.

---
+++ migrations/20221109121039_create_roles.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("roles", (knex) => {
+    knex.increments("id").primary();
+    knex.string("name", 150).notNullable().unique();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable("roles");
+};
