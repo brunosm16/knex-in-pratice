d83b3514f9eaf32478d1b04ecc0efd32fa2a697a
brunosm16 <brunoskr23@gmail.com>
2022-11-09 09:05:36 -0300

    chore: knexfile config.

---
+++ knexfile.js
@@ -0,0 +1,18 @@
+const isProduction = process.env.NODE_ENV !== "production";
+if (isProduction) require("dotenv").config();
+
+module.exports = {
+  client: "mysql2",
+  connection: {
+    database: "knex_in_practice",
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+  },
+  pool: {
+    min: 2,
+    max: 10,
+  },
+  migrations: {
+    tableName: "knex_migrations",
+  },
+};
